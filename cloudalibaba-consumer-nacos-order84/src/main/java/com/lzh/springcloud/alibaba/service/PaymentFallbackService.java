08255ec351adc4d9c52f39ddf3e3fdccbcf84f7f
30491 <304911790@qq.com>
2022-09-20 17:31:19 +0800

    添加sentinel

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/lzh/springcloud/alibaba/service/PaymentFallbackService.java
@@ -0,0 +1,16 @@
+package com.lzh.springcloud.alibaba.service;
+
+import com.lzh.springcloud.entities.CommonResult;
+import com.lzh.springcloud.entities.Payment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentFallbackService implements PaymentService
+{
+
+
+    @Override
+    public CommonResult<Payment> paymentSQL(Long id) {
+        return new CommonResult<>(44444,"服务降级返回,---PaymentFallbackService",new Payment(id,"errorSerial"));
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/lzh/springcloud/alibaba/controller/FlowLimitController.java
@@ -0,0 +1,33 @@
+package com.lzh.springcloud.alibaba.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class FlowLimitController {
+    @GetMapping("/testA")
+    public String testA() {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        log.info(Thread.currentThread().getName() + "\t" + "...testB");
+        return "------testB";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false)String p1,
+                             @RequestParam(value = "p2",required = false)String p2){
+        return "------testHotKty";
+    }
+    public String deal_testHotKey(String p1, String p2, BlockException exception){
+        return "------testHotKty,o(T~~T)o";
+    }
+}
